f0e3d0c8710bed7f3cc39593a8d876a8ef681d9d
Cyprien Behague <cyprien.behague@gmail.com>
2016-09-01 14:42:03 +0200

    BITE

--- src/lesCases/Game.java
+++ src/lesCases/Game.java
@@ -6,6 +6,9 @@ public class Game {
 	public Game(){
 		char c;
 		
+		boolean b=true;
+		
+		
 		Serpent s1 = new Serpent(1, 8, 8);
 		Serpent s2 = new Serpent(2, 2, 2);
 		
@@ -17,17 +20,32 @@ public class Game {
 			while (c==' '){
 				c= new MonScanner().c;
 			}
+			System.out.println(b);
+			if(b){
 			
-			if(c=='z'){
-				s1.deplacer(Directions.NORD);
-			}else if (c=='q'){
-				s1.deplacer(Directions.OUEST);
-			}else if (c=='d'){
-				s1.deplacer(Directions.EST);
-			}else if (c=='s'){
-				s1.deplacer(Directions.SUD);
+				if(c=='z'){
+					s1.deplacer(Directions.NORD);
+				}else if (c=='q'){
+					s1.deplacer(Directions.OUEST);
+				}else if (c=='d'){
+					s1.deplacer(Directions.EST);
+				}else if (c=='s'){
+					s1.deplacer(Directions.SUD);
+				}
+				b=false;
+			}else {
+				if(c=='o'){
+					s2.deplacer(Directions.NORD);
+				}else if (c=='1'){
+					s2.deplacer(Directions.OUEST);
+				}else if (c=='3'){
+					s2.deplacer(Directions.EST);
+				}else if (c=='2'){
+					s2.deplacer(Directions.SUD);
+				}
+				b=!b;
+				
 			}
-			
 			
 		}
 	
