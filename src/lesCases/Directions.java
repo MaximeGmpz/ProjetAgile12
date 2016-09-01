de29648eb5be9fcaad89b821f46c66d799105fe0
Axel Coeuret <axel.coeuret@live.fr>
2016-09-01 10:12:38 +0200

    Deplacement vers le haut

---
+++ src/lesCases/Directions.java
@@ -0,0 +1,8 @@
+package lesCases;
+
+public class Directions {
+	public static final int NORD = 0 ;
+	public static final int EST = 1 ;
+	public static final int SUD = 2 ;
+	public static final int OUEST = 3 ;
+}
--- src/lesCases/test.java
+++ src/lesCases/test.java
@@ -1,11 +1,13 @@
 package lesCases;
 
 public class test {
+	public static Plateau plateau = new Plateau();
 
 	public static void main(String[] args) {
-		Plateau plateau = new Plateau();
-		plateau.placer(2, 3, new Corp(2, 2, 3));
+		Snacke python = new Snacke(1, 8, 8);
 		plateau.afficher();
+		python.deplacer(Directions.NORD);
+		plateau.afficher(); 
 
 	}
 
